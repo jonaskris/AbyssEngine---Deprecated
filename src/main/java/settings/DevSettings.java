13a4c9d0f220ff963787039c4d26fd5b478b27c6
jonas kristoffersen <31920690+jonaskris@users.noreply.github.com>
2018-10-12 23:39:24 +0200

    Added DevSettings class. Used for specifying settings used for development, such as drawing bounding boxes or axii.

---
+++ src/main/java/settings/DevSettings.java
@@ -0,0 +1,8 @@
+package settings;
+
+public class DevSettings {
+    public static boolean drawCollisionComponentLines = true;
+    public static double[] colorCollisionComponentLines = new double[]{1.0d, 0.0d, 1.0d, 1.0d};
+
+    public static boolean drawAxii = true;
+}
